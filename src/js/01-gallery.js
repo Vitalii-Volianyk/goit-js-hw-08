3c5ce8c4f0ae7dbd29b89317496ffc67bbcbfb2d
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-10-07 13:59:51 +0300

    Install

---
+++ src/js/01-gallery.js
@@ -0,0 +1,20 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+(() => {
+  const gallery = document.querySelector(".gallery");
+  let elements = "";
+  galleryItems.forEach(({preview,original,description}) => {
+    elements += `
+	<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>`;
+  });
+
+  gallery.insertAdjacentHTML('beforeend', elements);
+  var lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
+})();
