3d58f5aa37cdc1fb6f24558eac159fdd2ae87160
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-10-07 16:42:57 +0300

    Complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import Throttle from "lodash.throttle";
+(() => {
+	const iframe = document.querySelector("iframe");
+    const player = new Player(iframe);
 
+    player.play();
+	player.on("play", function () {
+		if (localStorage.getItem("videoplayer-current-time")) {
+			player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+		}
+	});
+	player.on("timeupdate", Throttle((data)=>{
+		localStorage.setItem("videoplayer-current-time", data.seconds);
+	}, 1000));
+})();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import Throttle from "lodash.throttle";
+
+(() => {
+	const form = document.querySelector(".feedback-form");
+	const email = document.querySelector("input");
+	const message = document.querySelector("textarea");
+	
+	let data={"email":"","message":""};
+
+	form.addEventListener("input", Throttle((event) => {
+		if (event.target.nodeName==="INPUT") {
+			data.email = event.target.value;
+		} else if (event.target.nodeName==="TEXTAREA") {
+			data.message = event.target.value;
+		}
+		if (data) {
+			localStorage.setItem("feedback-form-state", JSON.stringify(data));
+		}
+	}, 500));
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		console.log(data);
+		form.reset();
+		localStorage.removeItem("feedback-form-state");
+		data = {"email":"","message":""};
+	});
+	
+	if (localStorage.getItem("feedback-form-state")) { 
+		data = JSON.parse(localStorage.getItem("feedback-form-state"));
+	}
+	email.value = data.email;
+	message.value = data.message;
+})();
 
