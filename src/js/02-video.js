1e769017c2849bbc79c8eb8b4825ccbd3e195f90
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-10-07 19:33:05 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ import Throttle from "lodash.throttle";
     const player = new Player(iframe);
 
     player.play();
-	player.on("play", function () {
+	player.on("playing", function () {
 		if (localStorage.getItem("videoplayer-current-time")) {
 			player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 		}
